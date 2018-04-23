940f734352e8b629669050ef528a5e19532ebe4c
Yacine Petitprez <yacine.petitprez@ingedata.net>
2018-04-23 18:38:48 +0800

    Messing around with postinstall script

--- build/install.sh
+++ build/install.sh
@@ -2,7 +2,7 @@ echo "Building crystal-coverage binary"
 
 mkdir -p ../../bin
 
-crystal --release src/coverage/inject/cli.cr -o ../../bin/crystal-coverage
+crystal --release ../src/coverage/inject/cli.cr -o ../../bin/crystal-coverage
 
 echo "Completed. To test with coverage, run bin/crystal-coverage"
 echo "Get documentations on https://github.com/anykeyh/crystal-coverage"
