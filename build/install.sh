20a20d249b235d71efeb46c84d0dd9d18bf5b5dd
Yacine Petitprez <yacine.petitprez@ingedata.net>
2018-04-24 00:17:41 +0800

    Fix binary

--- build/install.sh
+++ build/install.sh
@@ -2,7 +2,7 @@ echo "Building crystal-coverage binary"
 
 mkdir -p ../../bin
 
-crystal build --release src/coverage/inject/cli.cr -o ../../bin/crystal-coverage
+crystal build --release src/coverage/cli.cr -o ../../bin/crystal-coverage
 
 echo "Completed. To test with coverage, run bin/crystal-coverage"
 echo "Get documentations on https://github.com/anykeyh/crystal-coverage"
