981cf0be2485a1509c15fe0bd63001f895c2c3bf
Jade D. Kharats <jade.kharats@gmail.com>
2018-10-04 16:54:00 +0200

    feat(refactor): remove unused build/install.sh

--- build/install.sh
+++
@@ -1,8 +0,0 @@
-echo "Building crystal-coverage binary"
-
-mkdir -p ../../bin
-
-crystal build --release src/coverage/cli.cr -o ../../bin/crystal-coverage
-
-echo "Completed. To test with coverage, run bin/crystal-coverage"
-echo "Get documentations on https://github.com/anykeyh/crystal-coverage"
